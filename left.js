33e7ecc33060e62d2426888a385f059be603ad98
Saeed Mojtabazadeh <mojtabazadeh@outlook.com>
2022-11-04 13:05:21 +0330

    Add files via upload

---
+++ left.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+res = "<div style=\" z-index:1000; position:absolute; left:0px; top:0px; border-width:0px; \">"
+res += "<img src=\"https://s25.picofile.com/file/8451468368/myflag4.png\" width=\"150\" height=\"94\" "
+res += "border=\"0\" usemap=\"#iranl\" /><map name=\"iranl\" id=\"iranl\"> "
+res += "<area shape=\"poly\" alt=\"ایران\" coords=\"1,1,169,1,169,169\" "
+res += "href=\"http://mojtabazadeh.ir\" target=\"_blank\" /></map></div>"
+document.write(res);
---
+++ right.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+res = "<div style=\" z-index:1000; position:absolute; right:10px; top:10px; border-width:0px; \">"
+res += "<img src=\"https://s24.picofile.com/file/8452253492/WhatsApp.png\" width=\"70\" height=\"70\" "
+res += "border=\"0\" usemap=\"#iranl\" /><map name=\"iranl\" id=\"iranl\"> "
+res += "<area shape=\"poly\" alt=\"ایران\" coords=\"1,1,169,1,169,169\" "
+res += "href=\"http://mojtabazadeh.ir\" target=\"_blank\" /></map></div>"
+document.write(res);
